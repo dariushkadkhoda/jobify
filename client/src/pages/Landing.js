d0404f06053c6f521755cb3f83ac17bac413fee5
dariush kadkhoda <dariushkadkhoda@gmail.com>
2022-12-24 07:13:18 -0800

    router

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,12 +1,13 @@
-import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
 import Wrapper from "../assets/wrappers/LandingPage";
+import {Logo} from "../components/";
+import { Link } from "react-router-dom";
 const Landing = () => {
   return (
     <Wrapper>
-      <nav>
-        <img src={logo} alt="jobify" className="logo" />
-      </nav>
+    <nav>
+      <Logo/>
+    </nav>
       <div className="container page">
         {/* info */}
         <div className="info">
@@ -19,7 +20,7 @@ const Landing = () => {
             Porro ipsa similique numquam, corporis eos dignissimos nihil
             exercitationem voluptatibus reprehenderit.
           </p>
-          <button className="btn btn-hero"> Login/Register</button>
+          <Link to="/register" className="btn btn-hero"> Login/Register</Link>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
